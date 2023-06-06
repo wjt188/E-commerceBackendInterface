cc0279a7aad485946d2a3a3e193b630b78284f3a
wjt188 <78445294+wjt188@users.noreply.github.com>
2023-06-06 14:38:30 +0800

    Update category.go

--- goods_servers/handler/category.go
+++ goods_servers/handler/category.go
@@ -17,10 +17,104 @@ import (
 //DeleteCategory(ctx context.Context, in *DeleteCategoryRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
 //UpdateCategory(ctx context.Context, in *CategoryInfoRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
 
-//实现获取数据库中三级目录的数据
+import (
+	"Shop/goods_servers/global"
+	"Shop/goods_servers/model"
+	"Shop/goods_servers/proto"
+	"context"
+	"encoding/json"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+//DeleteCategory(ctx context.Context, in *DeleteCategoryRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
+//UpdateCategory(ctx context.Context, in *CategoryInfoRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
+
+// 实现获取数据库中三级目录的数据
 func (s *GoodsServer) GetAllCategorysList(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*proto.CategoryListResponse, error) {
 	var categorys []model.Category
 	global.DB.Where(&model.Category{Level: 1}).Preload("SubCategory.SubCategory ").Find(&categorys)
 	b, _ := json.Marshal(&categorys)
 	return &proto.CategoryListResponse{JsonData: string(b)}, nil
+}
+
+// 获取子分类商品数据信息
+func (s *GoodsServer) GetSubCategory(ctx context.Context, req *proto.CategoryListRequest, opts ...grpc.CallOption) (*proto.SubCategoryListResponse, error) {
+	//首先判断检索的商品分类信息是否存在
+	var category model.Category
+	if result := global.DB.First(&category, req.Id); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "商品分类不存在")
+	}
+
+	categoryListResponse := proto.SubCategoryListResponse{}
+	categoryListResponse.Info = &proto.CategoryInfoResponse{
+		Id:             category.ID,
+		Name:           category.Name,
+		Level:          category.Level,
+		IsTab:          category.IsTab,
+		ParentCategory: category.ParentCategoryID,
+	}
+	var subCategorys []model.Category
+	var subCategoryResponse []*proto.CategoryInfoResponse
+	preloads := "SubCategory"
+	if category.Level == 1 {
+		preloads = "SubCategory.SubCategory "
+	}
+	global.DB.Where(&model.Category{ParentCategoryID: req.Id}).Preload(preloads).Find(&subCategorys)
+	for _, subCategory := range subCategorys {
+		subCategoryResponse = append(subCategoryResponse, &proto.CategoryInfoResponse{
+			Id:             subCategory.ID,
+			Name:           subCategory.Name,
+			Level:          subCategory.Level,
+			IsTab:          subCategory.IsTab,
+			ParentCategory: subCategory.ParentCategoryID,
+		})
+	}
+	categoryListResponse.SubCategorys = subCategoryResponse
+	return &categoryListResponse, nil
+}
+
+// 创建分类接口
+func (s *GoodsServer) CreateCategory(ctx context.Context, req *proto.CategoryInfoRequest, opts ...grpc.CallOption) (*proto.CategoryInfoResponse, error) {
+	category := model.Category{}
+	category.Name = req.Name
+	category.Level = req.Level
+	if req.Level != 1 {
+		category.ParentCategoryID = req.ParentCategory
+	}
+	category.IsTab = req.IsTab
+	global.DB.Save(&category)
+	return &proto.CategoryInfoResponse{Id: int32(category.ID)}, nil
+}
+
+// 删除分类接口
+func (s *GoodsServer) DeleteCategory(ctx context.Context, req *proto.DeleteCategoryRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	if result := global.DB.Delete(&model.Category{}, req.Id); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "商品分类不存在")
+	}
+	return &emptypb.Empty{}, nil
+}
+
+// 更新商品分类接口
+func (s *GoodsServer) UpdateCategory(ctx context.Context, req *proto.CategoryInfoRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	var category model.Category
+	if result := global.DB.First(&category, req.Id); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "商品分类不存在")
+	}
+	if req.Name != "" {
+		category.Name = req.Name
+	}
+	if req.ParentCategory != 0 {
+		category.ParentCategoryID = req.ParentCategory
+	}
+	if req.Level != 0 {
+		category.Level = req.Level
+	}
+	if req.IsTab {
+		category.IsTab = req.IsTab
+	}
+	global.DB.Save(&category)
+	return &emptypb.Empty{}, nil
 }
